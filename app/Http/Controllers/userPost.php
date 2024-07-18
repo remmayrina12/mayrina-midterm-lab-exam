95dcb1d2a903e745bb4b8912a2f1cb051cd6c7da
RemMayrina <remmayrina@gmail.com>
2024-07-18 10:57:18 +0800

    laravel Model Controller

---
+++ app/Http/Controllers/userPost.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\routes\web;
+use Illuminate\Http\Request;
+
+class userPost extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\userComment;
+use App\Http\userController;
+use App\Http\userPost;
 Route::get('/', function () {
     return view('welcome');
 });
